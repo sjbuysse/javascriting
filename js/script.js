554d2a72243a56f6be17822b009ffc7f64b7c8c0
Simon Buysse <buysse.simon@gmail.com>
2017-04-16 15:26:10 +1200

    add popup toggle

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         $('#popup').toggleClass("open");
     }
     function fileHandler(event){
-        var file = event.target.files[0] //event.target references the object that dispatched the event (so here it is the input element)
+        var file = event.target.files[0]; //event.target references the object that dispatched the event (so here it is the input element)
         var reader = new FileReader();
         reader.onload = function(event){
             var data = event.target.result; //again event.target references the object that dispatched the event (here it is reader).
