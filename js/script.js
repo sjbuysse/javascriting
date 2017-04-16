dc4f430bc3ec3b00f9285c3ffae7343f986f5482
Simon Buysse <buysse.simon@gmail.com>
2017-04-16 12:13:23 +1200

    Change background to google maps

--- js/script.js
+++ js/script.js
@@ -14,4 +14,10 @@ $(document).ready(function(){
     }
     $('#file').on("change",fileHandler);
 });
-
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
