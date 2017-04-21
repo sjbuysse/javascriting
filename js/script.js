722005233da34c9535a48ac9b68c32774760ece4
Simon Buysse <buysse.simon@gmail.com>
2017-04-22 11:56:03 +1200

    Revert "Add images style to popup"
    This reverts commit 3302e50afdca9450371d963c00cf0cfe1c071bc0.

--- js/script.js
+++ js/script.js
@@ -12,21 +12,22 @@ $(document).ready(function(){
         $('#create-drawer').toggleClass("create-drawer--open");
     }
     function fileHandler(event){
+        console.log("keis");
         var file = event.target.files[0]; //event.target references the object that dispatched the event (so here it is the input element)
         var reader = new FileReader();
         reader.onload = function(event){
             var data = event.target.result; //again event.target references the object that dispatched the event (here it is reader).
-            $("#previewImg").attr('src', data);
+            $("#preview-img").attr('src', data);
         };
         reader.readAsDataURL(file);
     }
 
     function addImgToInfo() {
-        $("#images-upload-btn").clone().attr('id', '').appendTo('.images');
+        $("#preview-img").clone().attr('id', '').appendTo('#image-wrapper');
     }
 
-    $('#popup').on("change", '#imageFile', fileHandler);
-    $('#images-upload-btn').on('click', addImgToInfo);
+    $('#popup').on("change", '#file', fileHandler);
+    $('#submit-img').on('click', addImgToInfo);
 });
 var map;
 function initMap() {
