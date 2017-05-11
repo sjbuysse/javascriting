a362632efebf239d716cf703af7d1c22a395d6d4
Simon Buysse <buysse.simon@gmail.com>
2017-05-11 16:12:46 +1200

    delete everyting

--- js/androidbug.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("resize", function() {
-    console.log("buggs");
-  if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
-      console.log("bugggy");
-     window.setTimeout(function() {
-        document.activeElement.scrollIntoViewIfNeeded();
-     },0);
-  }
-});
